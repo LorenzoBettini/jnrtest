d9d00c4e3043376c8373a954c29234cff9122813
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-01-12 18:01:39 +0100

    updated dependencies

--- io.github.lorenzobettini.jnrtest.core/src/test/java/io/github/lorenzobettini/jnrtest/core/AppTest.java
+++ io.github.lorenzobettini.jnrtest.core/src/test/java/io/github/lorenzobettini/jnrtest/core/AppTest.java
@@ -1,20 +1,18 @@
 package io.github.lorenzobettini.jnrtest.core;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
 }
