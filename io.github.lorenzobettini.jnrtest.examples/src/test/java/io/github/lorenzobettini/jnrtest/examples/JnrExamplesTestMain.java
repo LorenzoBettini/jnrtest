3557f911867a222cceba5786e03629906b4d252e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2023-02-15 12:08:25 +0100

    show aggregated results in the example

--- io.github.lorenzobettini.jnrtest.examples/src/test/java/io/github/lorenzobettini/jnrtest/examples/JnrExamplesTestMain.java
+++ io.github.lorenzobettini.jnrtest.examples/src/test/java/io/github/lorenzobettini/jnrtest/examples/JnrExamplesTestMain.java
@@ -1,13 +1,14 @@
 package io.github.lorenzobettini.jnrtest.examples;
 
 import io.github.lorenzobettini.jnrtest.core.JnrTestRecorder;
+import io.github.lorenzobettini.jnrtest.core.JnrTestResultAggregator;
 import io.github.lorenzobettini.jnrtest.core.JnrTestRunner;
 import io.github.lorenzobettini.jnrtest.core.JnrTestStandardReporter;
 
 public class JnrExamplesTestMain {
 
 	public static void main(String[] args) {
-		var recorder = new JnrTestRecorder();
+		var recorder = new JnrTestRecorder().withElapsedTime();
 		var runner = new JnrTestRunner()
 				.testCase(new FactorialJnrTestCase())
 				.testCase(new FactorialJnrParameterizedTestCase())
@@ -16,6 +17,7 @@ public class JnrExamplesTestMain {
 				.testListener(recorder)
 				.testListener(new JnrTestStandardReporter().withElapsedTime());
 		runner.execute();
+		System.out.println("\nResults:\n\n" + new JnrTestResultAggregator().aggregate(recorder));
 		if (!recorder.isSuccess())
 			throw new RuntimeException("There are test failures");
 	}
