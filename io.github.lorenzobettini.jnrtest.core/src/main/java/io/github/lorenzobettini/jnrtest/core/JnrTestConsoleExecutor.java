077a6f529492483eb42840c1ede56a36ecfb5808
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-05-06 13:53:48 +0200

    tabs instead of spaces

--- io.github.lorenzobettini.jnrtest.core/src/main/java/io/github/lorenzobettini/jnrtest/core/JnrTestConsoleExecutor.java
+++ io.github.lorenzobettini.jnrtest.core/src/main/java/io/github/lorenzobettini/jnrtest/core/JnrTestConsoleExecutor.java
@@ -8,60 +8,60 @@ package io.github.lorenzobettini.jnrtest.core;
  * @author Lorenzo Bettini
  */
 public class JnrTestConsoleExecutor {
-    private final JnrTestRunner runner;
-    private final JnrTestRecorder recorder;
-    private final JnrTestConsoleReporter reporter;
+	private final JnrTestRunner runner;
+	private final JnrTestRecorder recorder;
+	private final JnrTestConsoleReporter reporter;
 
-    /**
-     * Creates a new JnrTestConsoleExecutor with default recorder and reporter configured with elapsed time.
-     */
-    public JnrTestConsoleExecutor() {
-        this.recorder = new JnrTestRecorder().withElapsedTime();
-        this.reporter = new JnrTestConsoleReporter().withElapsedTime();
-        this.runner = new JnrTestRunner();
-        this.runner.testListener(recorder);
-        this.runner.testListener(reporter);
-    }
+	/**
+	 * Creates a new JnrTestConsoleExecutor with default recorder and reporter configured with elapsed time.
+	 */
+	public JnrTestConsoleExecutor() {
+		this.recorder = new JnrTestRecorder().withElapsedTime();
+		this.reporter = new JnrTestConsoleReporter().withElapsedTime();
+		this.runner = new JnrTestRunner();
+		this.runner.testListener(recorder);
+		this.runner.testListener(reporter);
+	}
 
-    /**
-     * Adds a test case to be executed.
-     *
-     * @param testCase the test case to add
-     * @return this instance for method chaining
-     */
-    public JnrTestConsoleExecutor testCase(JnrTestCase testCase) {
-        runner.testCase(testCase);
-        return this;
-    }
+	/**
+	 * Adds a test case to be executed.
+	 *
+	 * @param testCase the test case to add
+	 * @return this instance for method chaining
+	 */
+	public JnrTestConsoleExecutor testCase(JnrTestCase testCase) {
+		runner.testCase(testCase);
+		return this;
+	}
 
-    /**
-     * Adds a listener to the test execution.
-     *
-     * @param listener the listener to add
-     * @return this instance for method chaining
-     */
-    public JnrTestConsoleExecutor testListener(JnrTestListener listener) {
-        runner.testListener(listener);
-        return this;
-    }
+	/**
+	 * Adds a listener to the test execution.
+	 *
+	 * @param listener the listener to add
+	 * @return this instance for method chaining
+	 */
+	public JnrTestConsoleExecutor testListener(JnrTestListener listener) {
+		runner.testListener(listener);
+		return this;
+	}
 
-    /**
-     * Executes all test cases and prints the results.
-     *
-     * @return true if all tests passed, false otherwise
-     */
-    public boolean executeWithoutThrowing() {
-        runner.execute();
-        System.out.println("\nResults:\n\n" + new JnrTestResultAggregator().aggregate(recorder));
-        return recorder.isSuccess();
-    }
+	/**
+	 * Executes all test cases and prints the results.
+	 *
+	 * @return true if all tests passed, false otherwise
+	 */
+	public boolean executeWithoutThrowing() {
+		runner.execute();
+		System.out.println("\nResults:\n\n" + new JnrTestResultAggregator().aggregate(recorder));
+		return recorder.isSuccess();
+	}
 
-    /**
-     * Executes all test cases, prints the results, and throws an exception if any tests fail.
-     */
-    public void execute() {
-        if (!executeWithoutThrowing()) {
-            throw new RuntimeException("There are test failures");
-        }
-    }
+	/**
+	 * Executes all test cases, prints the results, and throws an exception if any tests fail.
+	 */
+	public void execute() {
+		if (!executeWithoutThrowing()) {
+			throw new RuntimeException("There are test failures");
+		}
+	}
 }
--- io.github.lorenzobettini.jnrtest.core/src/main/java/io/github/lorenzobettini/jnrtest/core/JnrTestConsoleParallelExecutor.java
+++ io.github.lorenzobettini.jnrtest.core/src/main/java/io/github/lorenzobettini/jnrtest/core/JnrTestConsoleParallelExecutor.java
@@ -10,62 +10,62 @@ package io.github.lorenzobettini.jnrtest.core;
  * @author Lorenzo Bettini
  */
 public class JnrTestConsoleParallelExecutor {
-    private final JnrTestParallelRunner runner;
-    private final JnrTestThreadSafeRecorder recorder;
-    private final JnrTestThreadSafeConsoleReporter reporter;
+	private final JnrTestParallelRunner runner;
+	private final JnrTestThreadSafeRecorder recorder;
+	private final JnrTestThreadSafeConsoleReporter reporter;
 
-    /**
-     * Creates a new JnrTestConsoleParallelExecutor with default thread-safe
-     * recorder and reporter configured with elapsed time.
-     */
-    public JnrTestConsoleParallelExecutor() {
-        this.recorder = new JnrTestThreadSafeRecorder().withElapsedTime();
-        this.reporter = new JnrTestThreadSafeConsoleReporter().withElapsedTime();
-        this.runner = new JnrTestParallelRunner();
-        this.runner.testListener(recorder);
-        this.runner.testListener(reporter);
-    }
+	/**
+	 * Creates a new JnrTestConsoleParallelExecutor with default thread-safe
+	 * recorder and reporter configured with elapsed time.
+	 */
+	public JnrTestConsoleParallelExecutor() {
+		this.recorder = new JnrTestThreadSafeRecorder().withElapsedTime();
+		this.reporter = new JnrTestThreadSafeConsoleReporter().withElapsedTime();
+		this.runner = new JnrTestParallelRunner();
+		this.runner.testListener(recorder);
+		this.runner.testListener(reporter);
+	}
 
-    /**
-     * Adds a test case to be executed.
-     *
-     * @param testCase the test case to add
-     * @return this instance for method chaining
-     */
-    public JnrTestConsoleParallelExecutor testCase(JnrTestCase testCase) {
-        runner.testCase(testCase);
-        return this;
-    }
+	/**
+	 * Adds a test case to be executed.
+	 *
+	 * @param testCase the test case to add
+	 * @return this instance for method chaining
+	 */
+	public JnrTestConsoleParallelExecutor testCase(JnrTestCase testCase) {
+		runner.testCase(testCase);
+		return this;
+	}
 
-    /**
-     * Adds a listener to the test execution.
-     *
-     * @param listener the listener to add
-     * @return this instance for method chaining
-     */
-    public JnrTestConsoleParallelExecutor testListener(JnrTestListener listener) {
-        runner.testListener(listener);
-        return this;
-    }
+	/**
+	 * Adds a listener to the test execution.
+	 *
+	 * @param listener the listener to add
+	 * @return this instance for method chaining
+	 */
+	public JnrTestConsoleParallelExecutor testListener(JnrTestListener listener) {
+		runner.testListener(listener);
+		return this;
+	}
 
-    /**
-     * Executes all test cases, prints the results, and throws an exception if any
-     * tests fail.
-     */
-    public void execute() {
-        if (!executeWithoutThrowing()) {
-            throw new RuntimeException("There are test failures");
-        }
-    }
+	/**
+	 * Executes all test cases and prints the results.
+	 *
+	 * @return true if all tests passed, false otherwise
+	 */
+	public boolean executeWithoutThrowing() {
+		runner.execute();
+		System.out.println("\nResults:\n\n" + new JnrTestResultAggregator().aggregate(recorder));
+		return recorder.isSuccess();
+	}
 
-    /**
-     * Executes all test cases and prints the results.
-     *
-     * @return true if all tests passed, false otherwise
-     */
-    public boolean executeWithoutThrowing() {
-        runner.execute();
-        System.out.println("\nResults:\n\n" + new JnrTestResultAggregator().aggregate(recorder));
-        return recorder.isSuccess();
-    }
+	/**
+	 * Executes all test cases, prints the results, and throws an exception if any
+	 * tests fail.
+	 */
+	public void execute() {
+		if (!executeWithoutThrowing()) {
+			throw new RuntimeException("There are test failures");
+		}
+	}
 }
