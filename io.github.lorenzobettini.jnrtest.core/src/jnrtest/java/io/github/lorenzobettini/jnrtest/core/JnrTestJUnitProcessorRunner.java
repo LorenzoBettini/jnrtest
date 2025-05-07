4c40307ec6cfffa65a3770d5abbcb226b74b668e
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-05-07 10:48:28 +0200

    removed try catch

--- io.github.lorenzobettini.jnrtest.core/src/jnrtest/java/io/github/lorenzobettini/jnrtest/core/JnrTestJUnitProcessorRunner.java
+++ io.github.lorenzobettini.jnrtest.core/src/jnrtest/java/io/github/lorenzobettini/jnrtest/core/JnrTestJUnitProcessorRunner.java
@@ -3,32 +3,33 @@ package io.github.lorenzobettini.jnrtest.core;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+/**
+ * This class is a runner for the JnrTestJUnitProcessor.
+ * It processes JUnit test files and generates JnrTest files.
+ * 
+ * @author Lorenzo Bettini
+ */
 public class JnrTestJUnitProcessorRunner {
 
-	public static void main(String[] args) {
-		try {
-			// Get the current directory
-			Path currentDir = Paths.get("").toAbsolutePath();
+	public static void main(String[] args) throws Exception {
+		// Get the current directory
+		Path currentDir = Paths.get("").toAbsolutePath();
 
-			// Source directory containing JUnit tests
-			Path sourceDirectory = Paths.get(currentDir.toString(), "io.github.lorenzobettini.jnrtest.core", "src",
-					"test", "java", "io", "github", "lorenzobettini", "jnrtest", "core");
+		// Source directory containing JUnit tests
+		Path sourceDirectory = Paths.get(currentDir.toString(), "io.github.lorenzobettini.jnrtest.core", "src",
+				"test", "java", "io", "github", "lorenzobettini", "jnrtest", "core");
 
-			// Output directory for generated JnrTest files
-			Path outputDirectory = Paths.get(currentDir.toString(), "io.github.lorenzobettini.jnrtest.core", "src",
-					"jnrtest", "java");
+		// Output directory for generated JnrTest files
+		Path outputDirectory = Paths.get(currentDir.toString(), "io.github.lorenzobettini.jnrtest.core", "src",
+				"jnrtest", "java");
 
-			System.out.println("Processing test files from: " + sourceDirectory);
-			System.out.println("Writing output to: " + outputDirectory);
+		System.out.println("Processing test files from: " + sourceDirectory);
+		System.out.println("Writing output to: " + outputDirectory);
 
-			// Create and run the processor
-			JnrTestJUnitProcessor processor = new JnrTestJUnitProcessor(sourceDirectory, outputDirectory);
-			processor.process();
+		// Create and run the processor
+		JnrTestJUnitProcessor processor = new JnrTestJUnitProcessor(sourceDirectory, outputDirectory);
+		processor.process();
 
-			System.out.println("Processing complete!");
-		} catch (Exception e) {
-			System.err.println("Error processing JUnit test files:");
-			e.printStackTrace();
-		}
+		System.out.println("Processing complete!");
 	}
 }
