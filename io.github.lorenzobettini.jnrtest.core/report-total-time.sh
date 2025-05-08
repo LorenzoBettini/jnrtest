de4f1e9e42ae9d97f89c95fee075e0af5be2f7f4
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-05-08 10:31:39 +0200

    use bash as executable

--- io.github.lorenzobettini.jnrtest.core/report-total-time.sh
+++ io.github.lorenzobettini.jnrtest.core/report-total-time.sh
@@ -3,4 +3,4 @@
 # Calculate total test execution time by summing only the time attributes from the testsuite elements
 echo -e "\n========================================"
 echo "TOTAL SUREFIRE EXECUTION TIME: $(awk '/<testsuite.*time=/ {match($0, /time="([0-9.]+)"/, arr); total+=arr[1]} END{printf "%.3f seconds", total}' target/surefire-reports/TEST-*.xml)"
-echo "========================================"
+echo "========================================\n"
