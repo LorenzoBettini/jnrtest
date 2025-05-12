0b9ccfae2387bb87f5a8ba90816fa3c06ee706c6
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-05-12 17:26:22 +0200

    tabs instead of spaces

--- io.github.lorenzobettini.jnrtest.core/src/main/java/io/github/lorenzobettini/jnrtest/core/JnrTestStatistics.java
+++ io.github.lorenzobettini.jnrtest.core/src/main/java/io/github/lorenzobettini/jnrtest/core/JnrTestStatistics.java
@@ -7,71 +7,71 @@ package io.github.lorenzobettini.jnrtest.core;
  */
 public class JnrTestStatistics {
 
-    private int succeeded;
-    private int failed;
-    private int errors;
-    private long startTime;
-    private long elapsedTime;
-    private long totalTime = 0;
-    private boolean withElapsedTime = false;
+	private int succeeded;
+	private int failed;
+	private int errors;
+	private long startTime;
+	private long elapsedTime;
+	private long totalTime = 0;
+	private boolean withElapsedTime = false;
 
-    public void reset() {
-        succeeded = 0;
-        failed = 0;
-        errors = 0;
-        totalTime = 0;
-    }
+	public void reset() {
+		succeeded = 0;
+		failed = 0;
+		errors = 0;
+		totalTime = 0;
+	}
 
-    public void startTimer() {
-        this.startTime = System.currentTimeMillis();
-    }
+	public void startTimer() {
+		this.startTime = System.currentTimeMillis();
+	}
 
-    public void stopTimer() {
-        this.elapsedTime = System.currentTimeMillis() - startTime;
-        this.totalTime += elapsedTime;
-    }
+	public void stopTimer() {
+		this.elapsedTime = System.currentTimeMillis() - startTime;
+		this.totalTime += elapsedTime;
+	}
 
-    public void incrementSucceeded() {
-        succeeded++;
-    }
+	public void incrementSucceeded() {
+		succeeded++;
+	}
 
-    public void incrementFailed() {
-        failed++;
-    }
+	public void incrementFailed() {
+		failed++;
+	}
 
-    public void incrementErrors() {
-        errors++;
-    }
+	public void incrementErrors() {
+		errors++;
+	}
 
-    public int getSucceeded() {
-        return succeeded;
-    }
+	public int getSucceeded() {
+		return succeeded;
+	}
 
-    public int getFailed() {
-        return failed;
-    }
+	public int getFailed() {
+		return failed;
+	}
 
-    public int getErrors() {
-        return errors;
-    }
+	public int getErrors() {
+		return errors;
+	}
 
-    public int getTotalTests() {
-        return succeeded + failed + errors;
-    }
+	public int getTotalTests() {
+		return succeeded + failed + errors;
+	}
 
-    public long getElapsedTime() {
-        return elapsedTime;
-    }
+	public long getElapsedTime() {
+		return elapsedTime;
+	}
 
-    public long getTotalTime() {
-        return totalTime;
-    }
+	public long getTotalTime() {
+		return totalTime;
+	}
 
-    public boolean isWithElapsedTime() {
-        return withElapsedTime;
-    }
+	public boolean isWithElapsedTime() {
+		return withElapsedTime;
+	}
 
-    public void setWithElapsedTime(boolean withElapsedTime) {
-        this.withElapsedTime = withElapsedTime;
-    }
+	public void setWithElapsedTime(boolean withElapsedTime) {
+		this.withElapsedTime = withElapsedTime;
+	}
 }
