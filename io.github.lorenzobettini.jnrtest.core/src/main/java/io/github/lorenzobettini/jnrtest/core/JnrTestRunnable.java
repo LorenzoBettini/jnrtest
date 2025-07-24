39df48d0a343b5360aee06584866f10e484af6fd
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2025-07-24 12:17:41 +0200

    updated Javadoc

--- io.github.lorenzobettini.jnrtest.core/src/main/java/io/github/lorenzobettini/jnrtest/core/JnrTestRunnable.java
+++ io.github.lorenzobettini.jnrtest.core/src/main/java/io/github/lorenzobettini/jnrtest/core/JnrTestRunnable.java
@@ -1,16 +1,22 @@
 package io.github.lorenzobettini.jnrtest.core;
 
 /**
- * A runnable test.
+ * A runnable specification.
+ * 
+ * This can represent a test case or a lifecycle code.
+ * 
+ * Differently from Java {@link Runnable}, this interface allows throwing
+ * exceptions, which is useful for test execution where exceptions may
+ * occur during the test run.
  * 
  * @author Lorenzo Bettini
  */
 @FunctionalInterface
 public interface JnrTestRunnable {
 	/**
-	 * Runs the test.
+	 * Runs the code.
 	 * 
-	 * @throws Exception If the test execution fails
+	 * @throws Exception any exception that may occur during the run
 	 */
 	void runTest() throws Exception; // NOSONAR
 }
